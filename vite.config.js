1b5e884c8bb4c86ce02bdd6f21efd5b135af710f
Emmanuel <emmanuelobinna62@gmail.com>
2025-12-17 16:01:19 -0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
- import { defineConfig } from 'vite'
- import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
- export default defineConfig({
-   plugins: [
+export default defineConfig({
+  base: './',
+  plugins: [
     tailwindcss(),
-   ],
- })
-
-
-
-
+  ],
+})
