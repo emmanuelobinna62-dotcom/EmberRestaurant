3e220d55f940e61a0e154d706ca9d0dbfc07149d
Emmanuel <emmanuelobinna62@gmail.com>
2026-01-17 13:44:03 -0800

    stop

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./menu.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Playfair Display"', 'Georgia', 'serif'],
+        sans: ['"Poppins"', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
